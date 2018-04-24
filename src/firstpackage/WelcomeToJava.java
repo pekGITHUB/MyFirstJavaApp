19958b20515d7b3fbf367ea8071accbadb40a9f3
Owner <Owner@Owner-PC>
2018-04-24 18:56:17 -0500

    April 24 Push

---
+++ src/firstpackage/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package firstpackage;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Here Is What's Being Printed");
+	}
+
+
+}
